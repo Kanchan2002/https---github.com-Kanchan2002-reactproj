2e50367982e2ab492065bd28be7af4b753f32be1
Kanchan tripathi <95993754+Kanchan2002@users.noreply.github.com>
2023-07-27 21:23:27 +0530

    Update Expense.js

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -3,15 +3,16 @@ import './expense.css'
 import ExpenseDate from './expenseDate';
 import ExpenseDetail from './ExpenseDetail';
 function Expense(props) {
-  const deleteitem = (e)=>{
-     let get_item = e.target.parentElement;
-     get_item.remove();
+let amount = props.amount;
+  const [amount2,setItem] = useState(amount);
+  const update_item= ()=>{
+         setItem('100$');
   }
   return (
     <div className='expense-item'>
         <ExpenseDate date= {props.date}/>
          <ExpenseDetail title={props.title} location={props.location} amount={props.amount} />
-         <button onClick={deleteitem} className='expense-item__price'>Delete</button>
+         <button onClick={update_item}  className='expense-item__price'>update</button>
     </div>
   )
 }
