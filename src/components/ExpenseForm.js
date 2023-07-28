3f99fe7087d9a2cef97344086948817e30cccf56
Kanchan tripathi <95993754+Kanchan2002@users.noreply.github.com>
2023-07-28 18:48:05 +0530

    Update ExpenseForm.js

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ExpenseForm.css';
 function ExpenseForm() {
-   const getTheformvalue = (e)=>{
-       console.log(e.target.value);
+    const[enteredtitle,settitle] = useState('');
+    const[enteredamount,setamount] = useState('');
+    const[entereddate,setdate] = useState('');
+   const getThetextvalue = (e)=>{
+       settitle(e.target.value);
+   }
+   const getTheamount= (e)=>{
+     setamount(e.target.value);
+   }
+   const getThedate = (e) => {
+        setdate(e.target.value);
    }
   return (
     <div>
@@ -10,15 +19,15 @@ function ExpenseForm() {
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
             <label>Title</label>
-            <input type='text' onChange={getTheformvalue}/>
+            <input type='text' onChange={getThetextvalue}/>
             </div>
             <div className='new-expense__control'>
             <label>Amount</label>
-            <input type='number' min='0.01' step='0.01' />
+            <input type='number' min='0.01' step='0.01' onChange={getTheamount}/>
             </div>
             <div className='new-expense__control'>
             <label>Date</label>
-            <input type='date' min='2019-01-01' max='2022-12-31' />
+            <input type='date' min='2019-01-01' max='2022-12-31' onChange={getThedate}/>
             </div>
         </div>
         <div className='new-expense__actions'>
